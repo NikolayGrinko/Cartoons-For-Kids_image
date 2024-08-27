ce27a12573ed465e39e436623fbb06590ca37de1
grinya37 <112849355+grinya37@users.noreply.github.com>
2024-08-27 19:07:32 +0300

    add NewGroup -  "CartoonsForKids_ViewController, Router, Presenter, Entity"

---
+++ Cartoons For Kids_image/CartoonsForKidsList/CartoonsForKids_Interactor.swift
@@ -0,0 +1,29 @@
+//
+//  CartoonsForKids_Interactor.swift
+//  Cartoons For Kids_image
+//
+//  Created by Николай Гринько on 27.08.2024.
+//
+
+import UIKit
+
+class CartoonsForKids_Interactor: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
